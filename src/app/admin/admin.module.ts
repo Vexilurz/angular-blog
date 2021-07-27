ca4ac8024571331161083bd7bf3bd9d018392f19
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 15:52:07 +0300

    15.9 Auth service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './shared/services/auth.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,23 +19,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CreatePageComponent,
     EditPageComponent
   ],
-  providers: [],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
-    {
-      path: '', component: AdminLayoutComponent, children: [
-        { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
-        { path: 'login', component: LoginPageComponent },
-        { path: 'dashboard', component: DashboardPageComponent },
-        { path: 'create', component: CreatePageComponent },
-        { path: 'post/:id/edit', component: EditPageComponent },
-      ]
-    }
-  ])
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
+          { path: 'login', component: LoginPageComponent },
+          { path: 'dashboard', component: DashboardPageComponent },
+          { path: 'create', component: CreatePageComponent },
+          { path: 'post/:id/edit', component: EditPageComponent },
+        ]
+      }
+    ])
   ],
   exports: [RouterModule],
+  providers: [AuthService],
 })
 export class AdminModule { }
