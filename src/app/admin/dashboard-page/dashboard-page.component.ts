cd1f77cc020d1aedcb052ce1974f947406fe5576
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 19:56:54 +0300

    15.18 Posts list

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from '../shared/interfaces';
 import { AuthService } from '../shared/services/auth.service';
 
 @Component({
@@ -6,11 +9,32 @@ import { AuthService } from '../shared/services/auth.service';
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  pSub: Subscription
 
-  ngOnInit(): void {
+  constructor(
+    private postsService: PostsService
+  ) { 
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  remove(id: string | undefined) {
+    if (id) {
+      
+    }
+  }
+  
+  ngOnInit(): void {    
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
   }
 
 }
