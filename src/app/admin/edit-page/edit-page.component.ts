819fd5bc76d9e2f652a25b0752ae850fdb1b07f8
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 20:35:44 +0300

    15.22 Post edit

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { PostsService } from 'src/app/shared/posts.service';
 import { Post } from '../shared/interfaces';
@@ -10,9 +11,13 @@ import { Post } from '../shared/interfaces';
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
   form!: FormGroup  
+  post!: Post
+  submitted = false
+
+  uSub!: Subscription
 
   constructor(
     private route: ActivatedRoute,
@@ -22,18 +27,35 @@ export class EditPageComponent implements OnInit {
   }
 
   submit() {
+    if (this.form.invalid) return;
 
+    this.submitted = true;
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false;
+    })
   }
 
   ngOnInit(): void {
     this.route.params.pipe(switchMap((params: Params) => {
       return this.postsService.getById(params['id'])
     })).subscribe((post: Post) => {
+      this.post = post
       this.form = new FormGroup({
         title: new FormControl(post.title, Validators.required),
         text: new FormControl(post.text, Validators.required),
       })
     })
+  }
+
+  ngOnDestroy(): void {
+    if (this.uSub) {
+      this.uSub.unsubscribe();
+    }
   }
 
 }
