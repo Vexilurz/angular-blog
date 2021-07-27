092c5e88b8e865fdb13d09b66d2054c2c15b3a11
Sergei Potekhin <vexilurz@gmail.com>
2021-07-27 16:12:36 +0300

    15.10 Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDsJc9QIaK57bgSZt1VPeDisjqq-CevwOs'
 };
